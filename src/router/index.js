987dff87f1dcc90156dac29287065d430595f27b
xinz <2474069068@qq.com>
2022-08-29 09:13:06 +0800

    feature(master): 添加路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,19 @@ import autoRoutes from './routes';
 // 安装路由
 Vue.use(Router);
 // 创建路由实例，并导出
-export default new Router({
+const router = new Router({
     routes: [...autoRoutes]
 });
+
+// 前置路由守卫
+router.beforeEach((to, from, next) => {
+    /* 路由发生变化修改页面title */
+    if (to.meta.title) {
+        document.title = to.meta.title;
+    } else {
+        document.title = '默认title';
+    }
+    next(true);
+});
+
+export default router;
